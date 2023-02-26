4da3f94d661a97e2438f83afc56295ed389e7df5
Anita Rostomyan <info@tfnlab.com>
2023-02-26 00:24:32 -0800

    update

--- src/components/AccountDetails/AccountDetails.jsx
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -4,11 +4,11 @@ const AccountDetails = ({ accountAddress, accountBalance }) => {
   return (
     <div>
       <div className="jumbotron">
-        <h1 className="display-5">Burbank Live</h1>
+        <h1 className="display-5">Meme Genesis</h1>
         <div class="card col-md-12" >
           <div class="card-body">
             <p className="lead">
-                  Click the mint button above to  Mint a Burbank Live Token
+                  Click the mint button above to  Mint a Meme Genesis Token
                   <hr className="my-4" />
 
             </p>
